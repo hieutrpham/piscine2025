e45c359393f85f395c8194cdfb3b0f7d6d3b0ab8
hieutrpham <hieutrungpham91@gmail.com>
2025-03-22 07:01:02 +0200

    fix 06 add 07

--- c12/ex06/ft_list_clear.c
+++ c12/ex06/ft_list_clear.c
@@ -8,7 +8,7 @@ void    ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
     t_list  *next;
 
     current = begin_list;
-    while (!current)
+    while (current)
     {
         next = current->next;
         free_fct(current->data);
---
+++ c12/ex07/ft_list_at.c
@@ -0,0 +1,5 @@
+#include "ft_list.h"
+t_list  *ft_list_at(t_list *begin_list, unsigned int nbr)
+{
+
+}
