a842552ea86240482161f1e5332e53394f852e86
hieutrpham <hieutrungpham91@gmail.com>
2025-03-29 15:06:40 +0200

    ex14

--- c12/ex14/ft_list_sort.c
+++ c12/ex14/ft_list_sort.c
@@ -2,44 +2,35 @@
 
 void	ft_list_sort(t_list **begin_list, int (*cmp)(void *, void *))
 {
-	t_list	*one;
-	t_list	*two;
 	t_list	*prev;
 	t_list	*current;
 	t_list	*next;
 
-	one = *begin_list;
-	if (!one || !one->next)
-		return;
-	two = one->next;
-	if (cmp(one->data, two->data) > 0)
-	{
-		one->next = two->next;
-		two->next = one;
-		*begin_list = two;
-	}
-	prev = two;
-	current = one;
 	int swap = 1;
 	while (swap)
 	{
 		swap = 0;
 		current = *begin_list;
-	}
-	while (current->next)
-	{
-		next = current->next;
-		if (cmp(current->data, next->data) <= 0)
+		prev = NULL;
+		while (current->next)
 		{
-			prev = current;
-			current = next;
-		}
-		else
-		{
-			prev->next = current->next;
-			prev = current->next;
-			current->next = next->next;
-			next->next = current;
+			next = current->next;
+			if (cmp(current->data, next->data) > 0)
+			{
+				if (prev)
+					prev->next = next;
+				else // if prev is NULL then head needs swapped
+					*begin_list = next;
+				current->next = next->next;
+				next->next = current;
+				prev = next;
+				swap = 1;
+			}
+			else
+			{
+				prev = current;
+				current = current->next;
+			}
 		}
 	}
 }
