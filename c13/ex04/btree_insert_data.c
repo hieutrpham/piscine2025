7229dad1afa425419194e6396eb37036812fbd86
hieutrpham <hieutrungpham91@gmail.com>
2025-04-01 09:15:06 +0300

    update ex04

--- c13/ex04/btree_insert_data.c
+++ c13/ex04/btree_insert_data.c
@@ -3,21 +3,27 @@
 void	btree_insert_data(t_btree **root, void *item, int (*cmpf)(void *, void *))
 {
 	t_btree	*node;
-
+	
+    if (!root || !cmpf)
+		return;
+    if (!*root)
+	{
+		*root = btree_create_node(item);
+		return;
+    }
 	node = *root;
-	if (cmpf(item, node->item) > 0)
+	if (cmpf(item, node->item) >= 0)
 	{
 		if (node->right == NULL)
 			node->right = btree_create_node(item);
+		else
+			btree_insert_data(&node->right, item, cmpf);
 	}
-	if (cmpf(item, node->item) == 0)
+	if (cmpf(item, node->item) < 0)
 	{
-		if (node->right == NULL)
-			node->right = btree_create_node(item);
-	}
-	if (cmpf(item, node->item) > 0)
-	{
-		if (node->right == NULL)
-			node->right = btree_create_node(item);
+		if (node->left == NULL)
+			node->left = btree_create_node(item);
+		else
+			btree_insert_data(&node->left, item, cmpf);
 	}
 }
