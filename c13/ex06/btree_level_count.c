1a73b154b10b6f3b6aadd924e06727549fa26845
hieutrpham <hieutrungpham91@gmail.com>
2025-04-03 13:19:58 +0300

    ex06

--- c13/ex06/btree_level_count.c
+++ c13/ex06/btree_level_count.c
@@ -1,19 +1,21 @@
 #include "../ex00/btree_create_node.c"
-
-int	taso = 0;
+#include <stdio.h>
 
 int	btree_level_count(t_btree *root)
 {
 	if (!root)
 		return 0;
-	taso += 1;
-	btree_level_count(root->left);
-	btree_level_count(root->right);
-	return taso;
+	int left = btree_level_count(root->left);
+	printf("left\n");
+	int right = btree_level_count(root->right);
+	printf("right\n");
+	if (left > right)
+		return left + 1;
+	else
+		return right + 1;
 }
 
 t_btree	*btree_create_node(void *item);
-#include <stdio.h>
 int main()
 {
 	t_btree *node1 = btree_create_node("hello");
@@ -21,15 +23,11 @@ int main()
 	t_btree *node3 = btree_create_node("hello");
 	t_btree *node4 = btree_create_node("hello");
 	t_btree *node5 = btree_create_node("hello");
-	t_btree *node6 = btree_create_node("hello");
-	t_btree *node7 = btree_create_node("hello");
 	
 	node1->left = node2;
 	node1->right = node3;
 	node2->left = node4;
 	node2->right = node5;
-	node3->right = node6;
-	node5->right = node7;
 	printf("%d\n", btree_level_count(node1));
 	/*printf("%d\n", btree_level_count(node2));*/
 }
